62f0ae90530f8fb2dd077e4b86d4eadb81b59a7c
Hurderella <hurderella@gmail.com>
2016-03-23 21:28:05 +0900

    regene token key

--- slack_exam1.py
+++ slack_exam1.py
@@ -3,19 +3,41 @@
 import urllib
 import urllib2
 import time 
-url = "https://slack.com/api/rtm.start"
-values = {'token' : 'xoxb-27393677335-RrnsR3aCwbj89hJrPUUNAv2y'}
+import json
+import tornado.websocket
+from tornado import gen
 
-data = urllib.urlencode(values)
-req = urllib2.Request(url, data)
-response = urllib2.urlopen(req)
-#k = 0
-#while k != 5:
-#	the_path = response.read()
-#	print(the_path)
-#	k += 1
-#	time.sleep(5)
+URL = ""
 
-the_path = response.read()
-print(the_path)
+@gen.coroutine
+def connection_and_waiting():
+	print("URL : " + URL)
+	client = yield tornado.websocket.websocket_connect(URL)
+	#client.write_message("testing from client")
+	while True:
+		msg = yield client.read_message()
+		print("msg is : %s" % msg)
+
+if __name__ == "__main__":
+	rtm_url = "https://slack.com/api/rtm.start"
+	values = {'token' : 'tokenkey'}
+
+	data = urllib.urlencode(values)
+	req = urllib2.Request(rtm_url, data)
+	response = urllib2.urlopen(req)
+	#k = 0
+	#while k != 5:
+	#	the_path = response.read()
+	#	print(the_path)
+	#	k += 1
+	#	time.sleep(5)
+
+	the_path = response.read()
+	print(">>" + the_path)
+	JS = json.loads(the_path)
+	print(">>" + JS['url'])
+	URL = JS['url']
+	print()
+
+	tornado.ioloop.IOLoop.instance().run_sync(connection_and_waiting)
 
